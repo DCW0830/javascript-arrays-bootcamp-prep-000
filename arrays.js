5e4f29da6fa8f4b36173b999710ab5914b094250
Daniel Walz <walzdan@gmail.com>
2018-02-28 03:42:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element) 
   return array
 }
-function destructivelyAddElementToEndOfArray(array, elment) 
+function destructivelyAddElementToEndOfArray(array, element) 
   {
   array.push(element)
   return array
